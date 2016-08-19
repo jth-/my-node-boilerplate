e44eb55c0b5f4a96e6bde33f40bf75f93abf1250
Jimmy Harrison <jtharrison91@gmail.com>
2016-08-19 19:18:42 -0400

    Add jspm, system.js for module importing, ES2015 transpilation.

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ nunjucks.configure('views', {
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
---
+++ public/app/index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+
+let test = 'world';
+console.log(`Hello ${test}!`);
